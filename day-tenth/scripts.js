1b78c27c3100bf266d75e9be10a0708c157bb9b0
Adrian <adrian_zinko@op.pl>
2020-03-17 19:54:48 +0100

    feat: handle skip buttons

--- day-tenth/scripts.js
+++ day-tenth/scripts.js
@@ -4,7 +4,7 @@ const progress = document.querySelector('.progress');
 const progressBar = document.querySelector('.progress__filled');
 const toggleButton = document.querySelector('.toggle');
 const ranges = document.querySelectorAll('.player__slider');
-const skipButtons = document.querySelector('[data-skip]');
+const skipButtons = document.querySelectorAll('[data-skip]');
 
 function togglePlay() {
     const method = video.paused ? video.play() : video.pause();
@@ -16,6 +16,14 @@ function updateButton() {
     toggleButton.textContent = icon;
 }
 
+function skipPart() {
+    video.currentTime += parseFloat(this.dataset.skip);
+}
+
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
+
+toggleButton.addEventListener('click', togglePlay);
+
+skipButtons.forEach(button => button.addEventListener('click', skipPart));
