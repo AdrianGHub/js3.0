f3bc4421d0ca7830ad432bbc8a1db62ca46573f8
Adrian <adrian_zinko@op.pl>
2020-03-17 19:30:40 +0100

    feat: query required DOM elements

---
+++ day-tenth/scripts.js
@@ -0,0 +1,8 @@
+const player = document.querySelector('.player');
+const video = document.querySelector('.viewer');
+const progress = document.querySelector('.progress');
+const progressBar = document.querySelector('.progress__filled');
+const toggleButton = document.querySelector('.toggle');
+const ranges = document.querySelectorAll('.player__slider');
+const skipButtons = document.querySelector('[data-skip]');
+
