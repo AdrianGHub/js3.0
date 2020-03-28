ea736fb73b39c4061f2eaff2aa02e51d2c010b15
Adrian <adrian_zinko@op.pl>
2020-03-28 21:38:02 +0100

    feat: display the video from local web cam

--- day-nineteenth/scripts.js
+++ day-nineteenth/scripts.js
@@ -3,3 +3,18 @@ const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+const photoButton = document.querySelector('.photo__button');
+
+
+
+function getVideo() {
+    navigator.mediaDevices.getUserMedia({video: true, audio: false}) 
+        .then(localMediaStream => {
+            video.srcObject = localMediaStream;
+            video.play();
+        })
+        .catch((err => {
+            console.error(`Hey! I've denied Web Cam, maybe you should not do it man 😐`, err)
+        }))
+}
+
