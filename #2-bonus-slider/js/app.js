93e3e607e9d9522920d2cd857cb5396c27d2ab27
Adrian <adrian_zinko@op.pl>
2020-04-19 10:00:35 +0200

    feat: query dom elements

---
+++ #2-bonus-slider/js/app.js
@@ -0,0 +1,20 @@
+import '../sass/style.scss'
+
+
+document.addEventListener('DOMContentLoaded', () => {
+
+const imagesContainerEl = document.querySelector('.slider__image-container');
+const firstImageEl = document.querySelector('.slider__image-container--first img');
+const secondImageEl = document.querySelector('.slider__image-container--second img');
+
+
+
+
+
+
+
+
+
+
+
+})
