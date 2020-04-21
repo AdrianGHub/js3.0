8e56d724343a9a4588cf3727d98f023b93237f21
Adrian <adrian_zinko@op.pl>
2020-04-21 13:02:37 +0200

    feat: display fetch data image

--- #3-bonus-fetch-api/js/app.js
+++ #3-bonus-fetch-api/js/app.js
@@ -1,1 +1,25 @@
 import '../sass/style.scss'
+
+
+function listOfBreeds() {
+    return fetch('https://dog.ceo/api/breeds/list/all')
+        .then(res => res.json())
+        .then(breeds => {
+            return breeds.message;
+        })
+        .catch(err => console.log('Ups...', err))
+}; 
+
+function getRandomImage() {
+    return fetch('https://dog.ceo/api/breeds/image/random')
+        .then(res => res.json())
+        .then(image => {
+            return image.message;
+        }) 
+        .catch(err => console.log('Ups...', err))
+}
+
+const imgTag = document.querySelector('img');
+
+getRandomImage()
+    .then((image) => imgTag.setAttribute('src', image));
