4a7c19491aaa173c7a6c7fd9eedce04d730d29fc
Adrian <adrian_zinko@op.pl>
2020-05-13 23:57:56 +0200

    feat: change image slide on keyup event

--- #5-bonus-class-based-image-slider/index.js
+++ #5-bonus-class-based-image-slider/index.js
@@ -1,75 +1,93 @@
 class Slider {
-    constructor(images) {
-        this.images = images;
-        this.slide = null;
-        this.prevBtn = null;
-        this.nextBtn = null;
-        this.image = null;
-        this.currentSlide = 0;
-        this.slideArrayLength = 0;
-        this.slideCaption = null;
+	constructor(images) {
+		this.images = images;
+		this.slide = null;
+		this.prevBtn = null;
+		this.nextBtn = null;
+		this.image = null;
+		this.currentSlide = 0;
+		this.slideArrayLength = 0;
+		this.slideCaption = null;
 
-        this.UISelectors = {
-            slide: '[data-slide]',
-            prevButton: '[data-button-prev]',
-            nextButton: '[data-button-next]'
+		this.UISelectors = {
+			slide: "[data-slide]",
+			prevButton: "[data-button-prev]",
+			nextButton: "[data-button-next]",
+		};
+	}
 
-        }
-    }
+	initializeSlider() {
+		this.slide = document.querySelector(this.UISelectors.slide);
+		this.prevBtn = document.querySelector(this.UISelectors.prevButton);
+		this.nextBtn = document.querySelector(this.UISelectors.nextButton);
 
-    initializeSlider() {
-        this.slide = document.querySelector(this.UISelectors.slide);
-        this.prevBtn = document.querySelector(this.UISelectors.prevButton);
-        this.nextBtn = document.querySelector(this.UISelectors.nextButton);
+		this.image = document.createElement("img");
+		this.image.setAttribute("class", "slide__image");
 
-        this.image = document.createElement('img');
-        this.image.setAttribute('class', 'slide__image');
+		this.setSlideAttributes(0);
 
-        this.setSlideAttributes(0);
+		this.slideArrayLength = this.images && this.images.length;
 
-        this.slideArrayLength = this.images && this.images.length;
+		this.slide.appendChild(this.image);
 
-        this.slide.appendChild(this.image);
+		this.slideCaption = document.createElement("figcaption");
+		this.addCaption();
+		this.slideCaption.setAttribute("class", "slide__caption");
+		this.slide.appendChild(this.slideCaption);
 
-        this.slideCaption = document.createElement('figcaption');
-        this.addCaption();
-        this.slideCaption.setAttribute('class', 'slide__caption');
-        this.slide.appendChild(this.slideCaption);
+		this.disableButtons();
+		this.addListeners();
+	}
 
-        this.disableButtons();
-        this.addListeners();
-    }
+	addListeners() {
+		this.prevBtn.addEventListener("click", () =>
+			this.changeSlide(this.currentSlide - 1)
+		);
+		this.nextBtn.addEventListener("click", () =>
+			this.changeSlide(this.currentSlide + 1)
+		);
 
-    addListeners() {
-        this.prevBtn.addEventListener('click', () => this.changeSlide(this.currentSlide -1));
-        this.nextBtn.addEventListener('click', () => this.changeSlide(this.currentSlide + 1))
-    }
+		document.addEventListener("keydown", (e) => {
+			if (e.keyCode === 37) {
+				this.changeSlide(this.currentSlide - 1);
+			} else if (e.keyCode === 39) {
+				this.changeSlide(this.currentSlide + 1);
+			}
+		});
+	}
 
-    disableButtons() {
-        this.currentSlide === 0 
-        ? this.prevBtn.setAttribute('disabled', true) 
-        : this.prevBtn.removeAttribute('disabled');
-        this.currentSlide === this.slideArrayLength - 1 
-        ? this.nextBtn.setAttribute('disabled', true) 
-        : this.nextBtn.removeAttribute('disabled');
-    }
+	disableButtons() {
+		this.currentSlide === 0
+			? this.prevBtn.setAttribute("disabled", true)
+			: this.prevBtn.removeAttribute("disabled");
+		this.currentSlide === this.slideArrayLength - 1
+			? this.nextBtn.setAttribute("disabled", true)
+			: this.nextBtn.removeAttribute("disabled");
+	}
 
+	changeSlide(index) {
+		if (index === -1 || index === this.slideArrayLength) return;
+		this.currentSlide = index;
 
-    changeSlide(index) {
-        this.currentSlide = index;
+		this.addCaption();
 
-        this.addCaption();
+		this.setSlideAttributes(index);
+		this.disableButtons();
+	}
 
-        this.setSlideAttributes(index);
-        this.disableButtons();
-    }
+	addCaption() {
+		this.slideCaption.innerText = `${this.currentSlide + 1}/${
+			this.slideArrayLength
+		}`;
+	}
 
-    addCaption() {
-        this.slideCaption.innerText = `${this.currentSlide + 1}/${this.slideArrayLength}`;
-    }
-
-    setSlideAttributes(index) {
-        this.image.setAttribute('src', Array.isArray(this.images) && this.images.length && this.images[index]);
-        this.image.setAttribute('alt', `Slide o numerze ${index + 1}`);
-    }
+	setSlideAttributes(index) {
+		this.image.setAttribute(
+			"src",
+			Array.isArray(this.images) &&
+				this.images.length &&
+				this.images[index]
+		);
+		this.image.setAttribute("alt", `Slide o numerze ${index + 1}`);
+	}
 }
