b90eca759e3cdc0963ccdb9fa5e926a6d6e9e6cd
Adrian <adrian_zinko@op.pl>
2020-05-13 23:44:53 +0200

    feat: create & init button listener events

--- #5-bonus-class-based-image-slider/index.js
+++ #5-bonus-class-based-image-slider/index.js
@@ -5,6 +5,8 @@ class Slider {
         this.prevBtn = null;
         this.nextBtn = null;
         this.image = null;
+        this.currentSlide = 0;
+        this.slideArrayLength = 0;
 
         this.UISelectors = {
             slide: '[data-slide]',
@@ -21,9 +23,40 @@ class Slider {
 
         this.image = document.createElement('img');
         this.image.setAttribute('class', 'slide__image');
-        this.image.setAttribute('src', this.images[0]);
+
+        this.setSlideAttributes(0);
+
+        this.slideArrayLength = this.images && this.images.length;
 
         this.slide.appendChild(this.image);
+        this.disableButtons();
+        this.addListeners();
     }
 
+    addListeners() {
+        this.prevBtn.addEventListener('click', () => this.changeSlide(this.currentSlide -1));
+        this.nextBtn.addEventListener('click', () => this.changeSlide(this.currentSlide + 1))
+    }
+
+    disableButtons() {
+        this.currentSlide === 0 
+        ? this.prevBtn.setAttribute('disabled', true) 
+        : this.prevBtn.removeAttribute('disabled');
+        this.currentSlide === this.slideArrayLength - 1 
+        ? this.nextBtn.setAttribute('disabled', true) 
+        : this.nextBtn.removeAttribute('disabled');
+    }
+
+
+    changeSlide(index) {
+        this.currentSlide = index;
+
+        this.setSlideAttributes(index);
+        this.disableButtons();
+    }
+
+    setSlideAttributes(index) {
+        this.image.setAttribute('src', Array.isArray(this.images) && this.images.length && this.images[index]);
+        this.image.setAttribute('alt', `Slide o numerze ${index + 1}`);
+    }
 }
