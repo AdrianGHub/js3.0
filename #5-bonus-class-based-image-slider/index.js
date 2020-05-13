693194f0e4a6d452b199482ac9caa3b352869faf
Adrian <adrian_zinko@op.pl>
2020-05-13 20:38:02 +0200

    feat: query dom elements with initialize function

--- #5-bonus-class-based-image-slider/index.js
+++ #5-bonus-class-based-image-slider/index.js
@@ -1,5 +1,22 @@
 class Slider {
     constructor(images) {
         this.images = images;
+        this.slide = null;
+        this.prevBtn = null;
+        this.nextBtn = null;
+
+        this.UISelectors = {
+            slide: '[data-slide]',
+            prevButton: '[data-button-prev]',
+            nextButton: '[data-button-next]'
+
+        }
+    }
+
+    initializeSlider() {
+        this.slide = document.querySelector(this.UISelectors.slide);
+        this.prevBtn = document.querySelector(this.UISelectors.prevButton);
+        this.nextBtn = document.querySelector(this.UISelectors.nextButton);
+
     }
 }
