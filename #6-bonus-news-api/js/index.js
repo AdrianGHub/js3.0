aae0b1dc0f7e165bc496ee86a28ced080f043c52
Adrian <adrian_zinko@op.pl>
2020-05-19 09:04:37 +0200

    feat: display articles data in html section

--- #6-bonus-news-api/js/index.js
+++ #6-bonus-news-api/js/index.js
@@ -3,21 +3,37 @@ const API_KEY = '5c4695678e324eb1bccad1f9e170f730';
 
 class PolishNews {
     constructor() {
-        this.API = "http://newsapi.org/"
-        this.API_VERSION = 'v2'
-        this.API_RESOURCE = `top-headlines?country=pl&apiKey=${API_KEY}`
+        this.articlesArray = [];
 
-        this.API_ENDPOINT = `${this.API}/${this.API_VERSION}/${this.API_RESOURCE}`
+        this.articlesCatalog = null;
+
+
+        this.API = "http://newsapi.org/";
+        this.API_VERSION = 'v2';
+        this.API_RESOURCE = `top-headlines?country=pl&apiKey=${API_KEY}`;
+
+        this.API_ENDPOINT = `${this.API}/${this.API_VERSION}/${this.API_RESOURCE}`;
+
+        this.UISelectors = {
+            content: '[data-content]'
+        }
     }
 
-    async initializeArticles() {
+    initializeArticlesCatalog() {
+        this.articlesCatalog = document.querySelector(this.UISelectors.content);
+
         this.pullArticles()
     }
 
     async pullArticles() {
-        const {articles} = await this.fetchData(this.API_ENDPOINT);
+        const { articles } = await this.fetchData(this.API_ENDPOINT);
+
+        this.articlesArray = [...articles];
+
+        this.createArticlesCatalog(this.articlesArray);
 
         console.log(articles);
+
     }
 
     async fetchData(url) {
@@ -25,6 +41,18 @@ class PolishNews {
         const parsedResponse = await response.json();
 
         return parsedResponse;
+    }
+
+    createArticlesCatalog(articles) {
+        this.articlesCatalog.innerHTML += [
+            articles.map(article => {
+                if(article.author) {
+                    return `Autor artykułu: ${article.author}`
+                } else {
+                    return 'Brak autora 😔'
+                }
+            })
+        ]
     }
 }
 
