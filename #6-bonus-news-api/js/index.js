9b616e53429a19af78ed8dd6d884a5d65a29fde7
Adrian <adrian_zinko@op.pl>
2020-05-19 14:38:36 +0200

    feat: update fetched query & theme content

--- #6-bonus-news-api/js/index.js
+++ #6-bonus-news-api/js/index.js
@@ -2,13 +2,14 @@ const API_KEY = "5c4695678e324eb1bccad1f9e170f730";
 
 class PolishNews {
 	constructor() {
+		this.pageSize = 12;
+		this.currentPage = 1;
 		this.articlesArray = [];
-
 		this.articlesCatalog = null;
 
 		this.API = "http://newsapi.org/";
 		this.API_VERSION = "v2";
-		this.API_RESOURCE = `top-headlines?country=pl&apiKey=${API_KEY}`;
+		this.API_RESOURCE = `everything?q=tech&apiKey=${API_KEY}`;
 
 		this.API_ENDPOINT = `${this.API}/${this.API_VERSION}/${this.API_RESOURCE}`;
 
@@ -24,7 +25,7 @@ class PolishNews {
 	}
 
 	async pullArticles() {
-		const { articles } = await this.fetchData(this.API_ENDPOINT);
+		const { articles } = await this.fetchData(`${this.API_ENDPOINT}&pageSize=${this.pageSize}`);
 
 		this.articlesArray = [...articles];
 
