4399c8428761907943f8d4059a3f6e3b97911828
Adrian <adrian_zinko@op.pl>
2020-05-19 08:05:25 +0200

    feat: display fetched data in console

--- #6-bonus-news-api/js/index.js
+++ #6-bonus-news-api/js/index.js
@@ -1,1 +1,20 @@
 const API_KEY = '5c4695678e324eb1bccad1f9e170f730';
+
+
+class PolishNews {
+    constructor() {
+        this.API = "http://newsapi.org/"
+        this.API_VERSION = 'v2'
+        this.API_RESOURCE = `top-headlines?country=pl&apiKey=${API_KEY}`
+
+        this.API_ENDPOINT = `${this.API}/${this.API_VERSION}/${this.API_RESOURCE}`
+    }
+
+    async initializeArticles() {
+        const response = await fetch(this.API_ENDPOINT);
+        const parsedResponse = await response.json();
+
+        console.log(parsedResponse);
+    }
+}
+
