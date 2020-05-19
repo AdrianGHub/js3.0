035532792c103ffea6f2dc51fc97c6a4de4b896e
Adrian <adrian_zinko@op.pl>
2020-05-19 13:11:51 +0200

    feat: style article cards

--- #6-bonus-news-api/js/index.js
+++ #6-bonus-news-api/js/index.js
@@ -1,68 +1,78 @@
-const API_KEY = '5c4695678e324eb1bccad1f9e170f730';
-
+const API_KEY = "5c4695678e324eb1bccad1f9e170f730";
 
 class PolishNews {
-    constructor() {
-        this.articlesArray = [];
+	constructor() {
+		this.articlesArray = [];
 
-        this.articlesCatalog = null;
+		this.articlesCatalog = null;
 
+		this.API = "http://newsapi.org/";
+		this.API_VERSION = "v2";
+		this.API_RESOURCE = `top-headlines?country=pl&apiKey=${API_KEY}`;
 
-        this.API = "http://newsapi.org/";
-        this.API_VERSION = 'v2';
-        this.API_RESOURCE = `top-headlines?country=pl&apiKey=${API_KEY}`;
+		this.API_ENDPOINT = `${this.API}/${this.API_VERSION}/${this.API_RESOURCE}`;
 
-        this.API_ENDPOINT = `${this.API}/${this.API_VERSION}/${this.API_RESOURCE}`;
+		this.UISelectors = {
+			content: "[data-content]",
+		};
+	}
 
-        this.UISelectors = {
-            content: '[data-content]'
-        }
-    }
+	initializeArticlesCatalog() {
+		this.articlesCatalog = document.querySelector(this.UISelectors.content);
 
-    initializeArticlesCatalog() {
-        this.articlesCatalog = document.querySelector(this.UISelectors.content);
+		this.pullArticles();
+	}
 
-        this.pullArticles()
-    }
+	async pullArticles() {
+		const { articles } = await this.fetchData(this.API_ENDPOINT);
 
-    async pullArticles() {
-        const { articles } = await this.fetchData(this.API_ENDPOINT);
+		this.articlesArray = [...articles];
 
-        this.articlesArray = [...articles];
+		this.createArticlesCatalog(this.articlesArray);
 
-        this.createArticlesCatalog(this.articlesArray);
+		// console.log(articles);
+	}
 
-        // console.log(articles);
+	async fetchData(url) {
+		const response = await fetch(url);
+		const parsedResponse = await response.json();
 
-    }
+		return parsedResponse;
+	}
 
-    async fetchData(url) {
-        const response = await fetch(url);
-        const parsedResponse = await response.json();
+	createArticlesCatalog(articles) {
+		this.articlesCatalog.innerHTML += [
+			articles.map((article) => this.createArticle(article)).join(""),
+		];
+	}
 
-        return parsedResponse;
-    }
-
-    createArticlesCatalog(articles) {
-        this.articlesCatalog.innerHTML += [
-            articles.map(article => this.createArticle(article)).join('')
-        ];
-    }
-
-    createArticle({source:{name},author, urlToImage, publishedAt, content, url}) {
-        return `
+	createArticle({
+		source: { name },
+		author,
+        urlToImage,
+        title,
+		publishedAt,
+		content,
+		url,
+	}) {
+		return `
             <article class="article">
                 <header class="article__header">
                     <h2 class="article__heading">
-                        ${name} - ${author ? author : 'Brak autora 😔'}
+                        ${name}
                     </h2>
-                    <img class="article__image" src="${urlToImage}" alt="Zdjęcie artykułu.""/>
+                    <p class="article__author"> Autor: 
+                    ${author ? author : "Brak autora 😔"}
+                    </p>
+                    </header>
                     <p class="article__date">Data opublikowania: ${publishedAt}</p>
-                    <p class="article__content">${content ? content : 'Brak opisu artykułu 😟'}</p>
+                    <img class="article__image" src="${urlToImage}" alt="Zdjęcie artykułu.""/>
+                    <p class="article__title">${title}</p>
+                    <p class="article__content">${
+						content ? content : "Brak opisu artykułu 😟"
+					}</p>
                     <a class="article__link"href="${url}">Link do całego artykułu</a>
-                </header>
             </article>
-        `
-    }
+        `;
+	}
 }
-
