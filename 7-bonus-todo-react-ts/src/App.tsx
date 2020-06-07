81ad317dc54eda5350adf96d1674da94994bb02f
Adrian <adrian_zinko@op.pl>
2020-06-07 18:04:29 +0200

    feat: create function component for toggle todos

--- 7-bonus-todo-react-ts/src/App.tsx
+++ 7-bonus-todo-react-ts/src/App.tsx
@@ -1,7 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { TodoListItem } from './TodoListItem';
+
+const initialTodos: Todo[] = [
+  {
+    text: 'Wynieś śmieci na spacer',
+    complete: false
+  },
+  {
+    text: 'Oczyść regał główny w piwnicy',
+    complete: true
+  }
+
+];
+
+
 
 function App() {
-  return <>Hello World</>;
+  const [todos, setTodos] = useState(initialTodos);
+
+  const toggleTodo = (selectedTodo: Todo) => {
+    const newTodos = todos.map(todo => {
+      if (todo === selectedTodo) {
+        return {
+          ...todo, 
+          complete: !todo.complete,
+        };
+      }
+      return todo;
+    });
+    setTodos(newTodos);
+  };
+
+  return (
+      <ul>
+      <TodoListItem todo={todos[0]} toggleTodo={toggleTodo}/>  
+      <TodoListItem todo={todos[1]} toggleTodo={toggleTodo}/>  
+      </ul>
+    );
 }
 
 export default App;
