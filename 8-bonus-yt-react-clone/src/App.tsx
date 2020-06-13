237cb1430fc2df8f9d98e407b88654414ad8b864
Adrian <adrian_zinko@op.pl>
2020-06-13 22:32:58 +0200

    feat: fetch data from yt and display in console

--- 8-bonus-yt-react-clone/src/App.tsx
+++ 8-bonus-yt-react-clone/src/App.tsx
@@ -7,13 +7,26 @@ import { SearchBar, VideoDetails } from './components';
 import youtubeApi from './api/youtube';
 
 class App extends Component {
+
+    handleSubmit = async (searchTerm: string) => {
+        const response = await youtubeApi.get('search', { 
+            params: {
+                part: 'snippet',
+                maxResults: 5,
+                key: 'AIzaSyAtPYHoq5zCU6bIxVX8q28glXvhe1GSL-0',
+                q: searchTerm
+            }});
+            console.log(response.data);
+    }
+
     render () {
         return (
-            <Grid justify="center" container>
-                <Grid xs={12}>
-                    <Grid container>
+            <Grid justify="center" container spacing={6}>
+                <Grid item xs={12}>
+                    <Grid container spacing={6}>
                         <Grid item xs={12}>
-                            <SearchBar />
+                            <SearchBar
+                            onFormSubmit={this.handleSubmit} />
                         </Grid>
                         <Grid item xs={8}>
                             <VideoDetails />
--- 8-bonus-yt-react-clone/src/components/SearchBar.tsx
+++ 8-bonus-yt-react-clone/src/components/SearchBar.tsx
@@ -1,12 +1,36 @@
 import React, { Component } from 'react';
 
-class SearchBar extends Component {
+import { Paper, TextField } from '@material-ui/core';
+
+interface Props {
+    onFormSubmit: any;
+}
+
+
+class SearchBar extends Component<Props> {
     state = {
         searchTerm: '',
     };
+
+    handleChange = (event: any) => this.setState({ searchTerm: event.target.value});
+
+    handleSubmit = (event: any) => {
+        event.preventDefault();
+
+        const { searchTerm } = this.state;
+        const { onFormSubmit } = this.props;
+
+        onFormSubmit(searchTerm);
+
+    }
+
     render() {
         return (
-            <h1>Search Bar Component</h1>
+            <Paper elevation={6} style={{padding: '25px'}}>
+                <form onSubmit={this.handleSubmit}>
+                    <TextField fullWidth label="Search..." onChange={this.handleChange}/>   
+                </form>
+            </Paper>
         );
     }
 }
