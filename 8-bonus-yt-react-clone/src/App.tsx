a7eaacd9cc7f308dc8837797ac211a639d690572
Adrian <adrian_zinko@op.pl>
2020-06-14 11:33:27 +0200

    feat: display video based on search query

--- 8-bonus-yt-react-clone/src/App.tsx
+++ 8-bonus-yt-react-clone/src/App.tsx
@@ -8,6 +8,11 @@ import youtubeApi from './api/youtube';
 
 class App extends Component {
 
+    state = {
+        videos: [],
+        selectedVideo: null
+    }
+
     handleSubmit = async (searchTerm: string) => {
         const response = await youtubeApi.get('search', { 
             params: {
@@ -16,10 +21,12 @@ class App extends Component {
                 key: 'AIzaSyAtPYHoq5zCU6bIxVX8q28glXvhe1GSL-0',
                 q: searchTerm
             }});
-            console.log(response.data);
+        
+        this.setState({ videos: response.data.items, selectedVideo: response.data.items[0] });
     }
 
     render () {
+        const { selectedVideo } = this.state;
         return (
             <Grid justify="center" container spacing={6}>
                 <Grid item xs={12}>
@@ -29,7 +36,7 @@ class App extends Component {
                             onFormSubmit={this.handleSubmit} />
                         </Grid>
                         <Grid item xs={8}>
-                            <VideoDetails />
+                            <VideoDetails video={selectedVideo}/>
                         </Grid>
                         <Grid item xs={4}>
                             {/* VIDEO LIST  */}
--- 8-bonus-yt-react-clone/src/components/VideoDetails.tsx
+++ 8-bonus-yt-react-clone/src/components/VideoDetails.tsx
@@ -1,8 +1,25 @@
 import React from 'react';
+import { Paper, Typography } from '@material-ui/core';
 
-const videoDetails = () => {
+interface Props {
+    video: any;
+}
+
+const videoDetails: React.FC<Props> = ({ video }) => {
+    if(!video) return <div>Loading...</div>;
+
+    console.log(video.id.videoId);
+
+    const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`;
+
     return (
-        <h1>Video Details Component</h1>
+        <>
+            <Paper elevation={6} style={{ height: '70%' }}>
+                <iframe frameBorder="0" height="100%" width="100%" title="Movie Player" src={videoSrc}/>
+            </Paper>
+            <Paper elevation={6} style={{ padding: '15px' }}>
+            </Paper>
+        </>
     );
 };
 
