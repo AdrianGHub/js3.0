036c7b153cda9d57678d5d172cff36949b83fa0f
Adrian <adrian_zinko@op.pl>
2020-06-15 23:02:29 +0200

    feat: implement an error check with try catch block

--- 10-bonus-async-currency-converter/index.js
+++ 10-bonus-async-currency-converter/index.js
@@ -16,29 +16,43 @@ const getExchangeRate = async (fromCurrency, toCurrency) => {
 
 	const exchangeRate = pln * rate[toCurrency];
 
+	if (isNaN(exchangeRate)) {
+		throw new Error(
+			`Unable to get currency ${fromCurrency} and ${toCurrency}`
+		);
+	}
+
 	return exchangeRate;
 };
 
 getExchangeRate("USD", "PLN");
 
 const getCountries = async (toCurrency) => {
-	const response = await axios.get(
-		`https://restcountries.eu/rest/v2/currency/${toCurrency}`
-	);
+	try {
+		const response = await axios.get(
+			`https://restcountries.eu/rest/v2/currency/${toCurrency}`
+		);
 
-	return response.data.map((country) => country.name);
+		return response.data.map((country) => country.name);
+	} catch (error) {
+		throw new Error(`Unable to get countries that use ${toCurrency}`);
+	}
 };
 
 getCountries("PLN");
 
 const convertCurrency = async (fromCurrency, toCurrency, amount) => {
-	const exchangeRate = await getExchangeRate(fromCurrency, toCurrency);
 	const countries = await getCountries(toCurrency);
+	const exchangeRate = await getExchangeRate(fromCurrency, toCurrency);
 	const convertedAmount = (amount * exchangeRate).toFixed(2);
 
 	return `${amount} ${fromCurrency} is worth ${convertedAmount} ${toCurrency}. You can spent these in the following countries: ${countries}`;
 };
 
-convertCurrency("USD", "PLN", 30).then((message) => {
-	console.log(message);
-});
+convertCurrency("USD", "PLN", 30)
+	.then((message) => {
+		console.log(message);
+	})
+	.catch((error) => {
+		console.log(error.message);
+	});
