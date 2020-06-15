9637ab0879a5935f99f648d4b6cf400133264b28
Adrian <adrian_zinko@op.pl>
2020-06-15 22:24:47 +0200

    feat: init project & provide api key

---
+++ 10-bonus-async-currency-converter/index.js
@@ -0,0 +1,7 @@
+const axios = require("axios");
+
+const API_KEY = "b9922818ac116ec10b8d6a58e1e30696";
+
+// API's
+// Exchange Rate: http://data.fixer.io/api latest?access_key=b9922818ac116ec10b8d6a58e1e30696&format=1
+// Countries: https://restcountries.eu/rest/v2/currency/${currencyCode}
