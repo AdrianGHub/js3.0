d6d9c9c3803336943ffa1e420db0e55a259984b0
Adrian <adrian_zinko@op.pl>
2020-06-15 22:36:16 +0200

    feat: call get exchange rate func with async&await

--- 10-bonus-async-currency-converter/index.js
+++ 10-bonus-async-currency-converter/index.js
@@ -6,16 +6,17 @@ const API_KEY = "b9922818ac116ec10b8d6a58e1e30696";
 // Exchange Rate: http://data.fixer.io/api/ latest?access_key=b9922818ac116ec10b8d6a58e1e30696&format=1
 // Countries: https://restcountries.eu/rest/v2/currency/${currencyCode}
 
-const getExchangeRate = (fromCurrency, toCurrency) => {
-	axios
-		.get(
-			"http://data.fixer.io/api/latest?access_key=b9922818ac116ec10b8d6a58e1e30696&format=1"
-		)
-		.then((response) => {
-			const rate = response.data.rates;
+const getExchangeRate = async (fromCurrency, toCurrency) => {
+	const response = await axios.get(
+		"http://data.fixer.io/api/latest?access_key=b9922818ac116ec10b8d6a58e1e30696&format=1"
+	);
 
-			console.log(rate);
-		});
+	const rate = response.data.rates;
+	const pln = 1 / rate[fromCurrency];
+
+	const exchangeRate = pln * rate[toCurrency];
+
+	return exchangeRate;
 };
 
-getExchangeRate("USD", "EUR");
+getExchangeRate("USD", "PLN");
