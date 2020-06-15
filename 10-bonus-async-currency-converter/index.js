e3aa32f580c3dcc5b7f8727d52afa4574750f80b
Adrian <adrian_zinko@op.pl>
2020-06-15 22:29:27 +0200

    feat: provide get echange rate func and check fetched data

--- 10-bonus-async-currency-converter/index.js
+++ 10-bonus-async-currency-converter/index.js
@@ -3,5 +3,19 @@ const axios = require("axios");
 const API_KEY = "b9922818ac116ec10b8d6a58e1e30696";
 
 // API's
-// Exchange Rate: http://data.fixer.io/api latest?access_key=b9922818ac116ec10b8d6a58e1e30696&format=1
+// Exchange Rate: http://data.fixer.io/api/ latest?access_key=b9922818ac116ec10b8d6a58e1e30696&format=1
 // Countries: https://restcountries.eu/rest/v2/currency/${currencyCode}
+
+const getExchangeRate = (fromCurrency, toCurrency) => {
+	axios
+		.get(
+			"http://data.fixer.io/api/latest?access_key=b9922818ac116ec10b8d6a58e1e30696&format=1"
+		)
+		.then((response) => {
+			const rate = response.data.rates;
+
+			console.log(rate);
+		});
+};
+
+getExchangeRate("USD", "EUR");
