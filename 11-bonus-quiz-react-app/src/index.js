b24996981ea3bf54bb7e03d2c93928d5e9fdad77
Adrian <adrian_zinko@op.pl>
2020-06-17 09:30:07 +0200

    feat: use lifecycle method to fetch data from file

--- 11-bonus-quiz-react-app/src/index.js
+++ 11-bonus-quiz-react-app/src/index.js
@@ -4,10 +4,31 @@ import "./assets/style.css";
 import quizService from "./quizService/index";
 
 class QuizApp extends Component {
+	state = {
+		questionBank: [],
+	};
+
+	getQuestions = () => {
+		quizService().then((question) => {
+			this.setState({
+				questionBank: question,
+			});
+		});
+	};
+	componentDidMount() {
+		this.getQuestions();
+	}
 	render() {
 		return (
 			<div className="container">
 				<div className="title">QuizApp</div>
+				{this.state.questionBank.length > 0 &&
+					this.state.questionBank.map(
+						({ question, answers, correct, questionId }) => (
+							<h4>{question})</h4>
+						)
+					)}
+				;
 			</div>
 		);
 	}
